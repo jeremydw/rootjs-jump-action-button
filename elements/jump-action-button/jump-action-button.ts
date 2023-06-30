9fe33c914e6e8715e12c90cc9b32f41696493a01
jeremydw <jeremydw@gmail.com>
2023-06-29 17:10:56 -0700

    1 file modified, 1 added

--- elements/jump-action-button/jump-action-button.ts
+++ elements/jump-action-button/jump-action-button.ts
@@ -1,17 +1,31 @@
 import { customElement, property } from 'lit/decorators.js';
 import { html, LitElement, unsafeCSS } from 'lit';
+import styles from './jump-action-button.scss?inline';
 
 @customElement('jump-action-button')
 export class JumpActionButton extends LitElement {
+  static styles = unsafeCSS(styles);
+
   @property({ type: Boolean, reflect: true })
   visible?: boolean;
 
+  connectedCallback() {
+    super.connectedCallback();
+  }
+
+  private testVisibility() {
+    const elementRect = this.parentElement.getBoundingClientRect();
+    const viewportHeight = window.innerHeight;
+    const elementBottom = elementRect.bottom;
+    return elementBottom < viewportHeight;
+  }
+
   override render() {
     return html`
-      <div class=${classMap({
+      <div class="frame"><div class=${classMap({
         container: 'container',
         'container--visible': this.visible,
-      })}><button>Learn more</button></div>
+      })}><button>Learn more</button></div></div>
     `;
   }
 }
