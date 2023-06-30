2982cd255a58f4338dd8dbbd0b825751237df116
jeremydw <jeremydw@gmail.com>
2023-06-29 17:30:59 -0700

    4 files modified, 1 added

--- elements/jump-action-button/jump-action-button.ts
+++ elements/jump-action-button/jump-action-button.ts
@@ -1,31 +1,67 @@
 import { customElement, property } from 'lit/decorators.js';
 import { html, LitElement, unsafeCSS } from 'lit';
 import styles from './jump-action-button.scss?inline';
+import { classMap } from 'lit/directives/class-map.js';
 
 @customElement('jump-action-button')
 export class JumpActionButton extends LitElement {
   static styles = unsafeCSS(styles);
 
   @property({ type: Boolean, reflect: true })
-  visible?: boolean;
+  visible: boolean = false;
+
+  @property()
+  href: string = undefined;
 
   connectedCallback() {
     super.connectedCallback();
+    window.addEventListener('scroll', this.updateVisibility.bind(this), {
+      passive: true,
+    });
+    window.addEventListener('resize', this.updateVisibility.bind(this), {
+      passive: true,
+    });
   }
 
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    window.removeEventListener('scroll', this.updateVisibility.bind(this));
+    window.removeEventListener('resize', this.updateVisibility.bind(this));
+  }
+
+  firstUpdated() {
+    // Test within firstUpdated. The parent isn't available until firstUpdated runs.
+    this.updateVisibility();
+  }
+
+  private updateVisibility() {
+    this.visible = this.testVisibility();
+    console.log('foo');
+  }
+
+  /**
+   * Tests whether the button's parent element's bottom is scrolled past the bottom
+   * of the viewport.
+   */
   private testVisibility() {
+    if (!this.parentElement) {
+      return;
+    }
     const elementRect = this.parentElement.getBoundingClientRect();
     const viewportHeight = window.innerHeight;
     const elementBottom = elementRect.bottom;
-    return elementBottom < viewportHeight;
+    return elementBottom > viewportHeight;
   }
 
   override render() {
     return html`
-      <div class="frame"><div class=${classMap({
-        container: 'container',
-        'container--visible': this.visible,
-      })}><button>Learn more</button></div></div>
+      <div class=${classMap({
+        frame: true,
+        'frame--visible': this.visible,
+      })}><div class=${classMap({
+      container: true,
+      'container--visible': this.visible,
+    })}><a class="button" href="${this.href}"><slot></slot></a></div></div>
     `;
   }
 }
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,13 +1,22 @@
+import './styles.scss';
 import ButtonGroup from '@/components/ButtonGroup';
 
 export function Page() {
   return (
     <>
-      <div style="text-align: center">
-        <ButtonGroup orientation="vertical">
-          <button>Lorem ipsum dolor sit amet</button>
-          <button>Lorem ipsum</button>
-        </ButtonGroup>
+      <div style="text-align: center; height: 680px; border: 1px dotted blue; padding: 20px; box-sizing: border-box;">
+        <h1>Hero Module</h1>
+        <p>
+          Jump action button only appears if the hero module's bottom is not
+          scrolled past the viewport bottom.
+        </p>
+        <jump-action-button href="#next">Explore more</jump-action-button>
+      </div>
+      <div
+        style="height: 1000px; border: 1px dotted green; padding: 20px; box-sizing: border-box;"
+        id="next"
+      >
+        Next Module
       </div>
     </>
   );
